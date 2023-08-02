5e1c61c8931f99ce316d5ca7de093baf5e8b7e5f
Mohamed Rizad <22ad063@excelcolleges.com>
2023-08-02 12:24:24 +0530

    Update settings.py

--- WeatherApp/settings.py
+++ WeatherApp/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+STATIC_URL = '/static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
